4bf4b0b51365630b43443c7506fa6f40629b91d2
mmiirandaaaa <maximiliano.mirandah@sansano.usm.cl>
2024-09-01 13:34:16 -0400

    agregado el merge sort

--- ordenamiento.cpp
+++ ordenamiento.cpp
@@ -26,14 +26,63 @@ void insertion_sort(int *arr, int n){
 	}
 }
 
+void merge(int *A, int l, int m, int r){
+	int nL = m-l+1; //largo de la mitad A[l:m] 
+	int nR = r-m; // largo de la mitad A[m+1:r]
+	int L[nL]; int R[nR];
+	
+	for(int i=0; i<nL; i++){
+		L[i] = A[l + i];
+	}
+	for(int i=0; i<nR; i++){
+		R[i] = A[m+1 + i];
+	}
+
+	int i=0; int j=0; int k=l;
+
+	while(i<nL && j<nR){
+		if(L[i] <= R[j]){
+			A[k] = L[i];
+			i++;
+		}
+		else{
+			A[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while(i<nL){
+		A[k] = L[i];
+		i++; k++;
+	}
+
+	while(j<nR){
+		A[k] = R[j];
+		j++; k++;
+	}
+
+}	
+
+
+void merge_sort(int *A, int l, int r){
+	if(l>=r){
+		return;
+	}
+	int m = l + (r - l) / 2;
+	merge_sort(A, l, m);
+	merge_sort(A, m+1, r);
+	merge(A, l,m,r);
+}
+
 
 
 int main(){
-	int a[5]={8,2,3,10,9};
-	int n=5;
+	int n=7;
+	int a[n]={20,7,1,3,4,10,15};
 	cout << "ARREGLO NO ORDENADO\n";
 	print(a,n);
-	insertion_sort(a,n);
+	merge_sort(a,0,n);
 	cout << "\nARREGLO ORDENADO\n";
 	print(a,n);
 
